ec5a8e392027a4b4c8a72afba549a3d52fe274e2
RafaellSouzaPinto <rafaelsouza.rs14@gmail.com>
2025-11-14 14:55:27 -0300

    Initial project setup

---
+++ src/main/java/workwell/WorkWell/WorkWellApplication.java
@@ -0,0 +1,13 @@
+package workwell.WorkWell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkWellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkWellApplication.class, args);
+	}
+
+}
---
+++ src/test/java/workwell/WorkWell/WorkWellApplicationTests.java
@@ -0,0 +1,13 @@
+package workwell.WorkWell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkWellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
